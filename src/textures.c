8b29b8ce7b533efe45fcea726f95d232185f4ae6
Manrique <manriquedv@estudiantec.cr>
2018-10-10 00:10:11 -0600

    Arreglillo

--- src/textures.c
+++ src/textures.c
@@ -18,13 +18,13 @@ Texel_Coord * map_sphere (Intersection * intersection){
     Sphere* sphere = (Sphere*) intersection -> object -> object;
     Texel_Coord  *uv = (Texel_Coord*) malloc(sizeof(Texel_Coord));
 
-    Vector distance_center = {intersection -> intersection_point.x - sphere->center.x,
+    Vector normal_vector = {intersection -> intersection_point.x - sphere->center.x,
                         0,
                         intersection -> intersection_point.z - sphere->center.z};
 
-    normalize_vector(&distance_center);
+    normalize_vector(&normal_vector);
 
-    uv -> u = acos(distance_center.z) / (2 * PI);
+    uv -> u = acos(normal_vector.z) / (2 * PI);
     uv -> v = (intersection -> intersection_point.y - sphere->center.y + sphere->radius)/ (2 * sphere->radius);
 
 
